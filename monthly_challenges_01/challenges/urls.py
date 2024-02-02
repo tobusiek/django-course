5440d8d5e49a397fd8c8a3354ca338d0483d2114
tobasqo <s.jasiek.gaming@gmail.com>
2024-02-02 22:36:46 +0100

    add basic pages

---
+++ monthly_challenges_01/challenges/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("january", views.january),
+    path("february", views.february),
+    path("march", views.march),
+]
--- monthly_challenges_01/challenges/views.py
+++ monthly_challenges_01/challenges/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def january(request: HttpRequest) -> HttpResponse:
+    return HttpResponse(b"Eat no meat for an entire month!")
+
+
+def february(request: HttpRequest) -> HttpResponse:
+    return HttpResponse(b"Walk for at least 20 minutes every day!")
+
+
+def march(request: HttpRequest) -> HttpResponse:
+    return HttpResponse(b"Learn Django for at least 20 minutes every day!")
