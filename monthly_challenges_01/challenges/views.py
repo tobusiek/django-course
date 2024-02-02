e39164864c95c1e4b1ae5f1a4b813d47b86fd4b3
tobasqo <s.jasiek.gaming@gmail.com>
2024-02-02 23:02:20 +0100

    factorization

--- monthly_challenges_01/challenges/views.py
+++ monthly_challenges_01/challenges/views.py
@@ -2,18 +2,30 @@ from django.http import HttpRequest, HttpResponse
 from django.http.response import HttpResponseNotFound
 from django.shortcuts import render
 
+MONTHLY_CHALLENGES: dict[str, bytes] = {
+    "january": b"Eat no meat for the entire month!",
+    "february": b"Walk for at least 20 minutes every day!",
+    "march": b"Learn Django for at least 20 minutes every day!",
+    "april": b"Eat no meat for the entire month!",
+    "may": b"Walk for at least 20 minutes every day!",
+    "june": b"Learn Django for at least 20 minutes every day!",
+    "july": b"Eat no meat for the entire month!",
+    "august": b"Walk for at least 20 minutes every day!",
+    "september": b"Eat no meat for the entire month!",
+    "october": b"Eat no meat for the entire month!",
+    "november": b"Walk for at least 20 minutes every day!",
+    "december": b"Eat no meat for the entire month!",
+}
+
 # Create your views here.
 
 
-def monthly_challenge(request: HttpRequest, month: str) -> HttpResponse:
-    challenge_text: bytes
-    if month == "january":
-        challenge_text = b"Eat no meat for the entire month!"
-    elif month == "february":
-        challenge_text = b"Walk for at least 20 minutes every day!"
-    elif month == "march":
-        challenge_text = b"Learn Django for at least 20 minutes every day!"
-    else:
-        return HttpResponseNotFound(b"This month is not supported!")
+def monthly_challenge_by_number(request: HttpRequest, month: int) -> HttpResponse:
+    return HttpResponse(bytes(month))
 
-    return HttpResponse(challenge_text)
+
+def monthly_challenge(request: HttpRequest, month: str) -> HttpResponse:
+    if month not in MONTHLY_CHALLENGES:
+        return HttpResponseNotFound(b"Wrong month spelling!")
+
+    return HttpResponse(MONTHLY_CHALLENGES[month])
