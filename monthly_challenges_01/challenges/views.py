1fcc3c517158241e69035b793302b0c6ec69137b
tobasqo <s.jasiek.gaming@gmail.com>
2024-02-02 22:45:19 +0100

    dynamic paths

--- monthly_challenges_01/challenges/views.py
+++ monthly_challenges_01/challenges/views.py
@@ -1,16 +1,19 @@
 from django.http import HttpRequest, HttpResponse
+from django.http.response import HttpResponseNotFound
 from django.shortcuts import render
 
 # Create your views here.
 
 
-def january(request: HttpRequest) -> HttpResponse:
-    return HttpResponse(b"Eat no meat for an entire month!")
+def monthly_challenge(request: HttpRequest, month: str) -> HttpResponse:
+    challenge_text: bytes
+    if month == "january":
+        challenge_text = b"Eat no meat for the entire month!"
+    elif month == "february":
+        challenge_text = b"Walk for at least 20 minutes every day!"
+    elif month == "march":
+        challenge_text = b"Learn Django for at least 20 minutes every day!"
+    else:
+        return HttpResponseNotFound(b"This month is not supported!")
 
-
-def february(request: HttpRequest) -> HttpResponse:
-    return HttpResponse(b"Walk for at least 20 minutes every day!")
-
-
-def march(request: HttpRequest) -> HttpResponse:
-    return HttpResponse(b"Learn Django for at least 20 minutes every day!")
+    return HttpResponse(challenge_text)
