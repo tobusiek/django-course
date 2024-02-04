d7bcfecc06df542574ad5f6aa5d19526f39d145c
tobasqo <s.jasiek.gaming@gmail.com>
2024-02-04 16:02:24 +0100

    add basic routing

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,1 +1,15 @@
+from django.http import HttpRequest
+
 # Create your views here.
+
+
+def starting_page(request: HttpRequest):
+    pass
+
+
+def posts(request: HttpRequest):
+    pass
+
+
+def post_detail(request: HttpRequest):
+    pass
