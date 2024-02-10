e39c02717c0abf03cb69c49c946e204a65559af2
tobasqo <s.jasiek.gaming@gmail.com>
2024-02-11 00:23:42 +0100

    add posting comment logic

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,6 +1,8 @@
-from typing import Any
-
-from django.views.generic import DetailView, ListView
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.views import View
+from django.views.generic import ListView
 
 from blog.forms import CommentForm
 from blog.models import Post
@@ -25,12 +27,32 @@ class AllPostsView(ListView):
     context_object_name = "all_posts"
 
 
-class SinglePostView(DetailView):
+class SinglePostView(View):
     template_name = "blog/post-detail.html"
     model = Post
 
-    def get_context_data(self, **kwargs) -> dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        context["comment_form"] = CommentForm()
-        return context
+    def get(self, request: HttpRequest, slug: str) -> HttpResponse:
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm(),
+        }
+        return render(request, "blog/post-detail.html", context)
+
+    def post(self, request: HttpRequest, slug: str) -> HttpResponse:
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("post-detail-page"), args=[slug])
+
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": comment_form,
+        }
+        return render(request, "blog/post-detail.html", context)
