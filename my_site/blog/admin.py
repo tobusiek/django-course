6a414a9693ee42fc73d1495fde6ad8867cc2af5d
tobasqo <s.jasiek.gaming@gmail.com>
2024-02-10 11:21:06 +0100

    add posts to db

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,1 +1,16 @@
+from django.contrib import admin
+
+from blog.models import Author, Post, Tag
+
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author", "date", "tags")
+    list_display = ("title", "author", "date")
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,1 +1,36 @@
+from django.core.validators import MinLengthValidator
+from django.db import models
+
 # Create your models here.
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+
+    def full_name(self) -> str:
+        return self.first_name + " " + self.last_name
+
+    def __str__(self) -> str:
+        return self.full_name()
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return self.caption
+
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, null=True, related_name="posts"
+    )
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Tag)
+    slug = models.SlugField(unique=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
