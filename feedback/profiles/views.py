45c0b941bb62ee82cb68a69a256472c937adc76b
tobasqo <s.jasiek.gaming@gmail.com>
2024-02-10 20:29:54 +0100

    refactor to create view

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,23 +1,12 @@
-from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
-from django.views import View
+from django.views.generic.edit import CreateView
 
-from profiles.forms import ProfileForm
 from profiles.models import UserProfile
 
 # Create your views here.
 
 
-class CreateProfileView(View):
-    def get(self, request: HttpRequest) -> HttpResponse:
-        return render(request, "profiles/create_profile.html", {"form": ProfileForm()})
-
-    def post(self, request: HttpRequest) -> HttpResponse:
-        form = ProfileForm(request.POST, request.FILES)
-
-        if form.is_valid():
-            profile = UserProfile(image=request.FILES["user_image"])
-            profile.save()
-            return HttpResponseRedirect("/profiles")
-
-        return render(request, "profiles/create_profile.html", {"form": form})
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
