3478c8bc5a1a6df0d5af3bc006f04d905d203673
tobasqo <s.jasiek.gaming@gmail.com>
2024-02-10 19:47:14 +0100

    naive file storage approach

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,3 +1,5 @@
+import os
+from django.core.files.uploadedfile import UploadedFile
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
@@ -5,10 +7,16 @@ from django.views import View
 # Create your views here.
 
 
+def store_file(file: UploadedFile) -> None:
+    with open(os.path.join(os.getcwd(), "temp", file.name), "wb+") as image_file:
+        for chunk in file.chunks():
+            image_file.write(chunk)
+
+
 class CreateProfileView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
         return render(request, "profiles/create_profile.html")
 
     def post(self, request: HttpRequest) -> HttpResponse:
-        print(request.FILES["image"])
+        store_file(request.FILES["image"])
         return HttpResponseRedirect("/profiles")
