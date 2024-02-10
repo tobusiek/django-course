dfee7d9491a1fd26827f3c9701f0ff24801835ae
tobasqo <s.jasiek.gaming@gmail.com>
2024-02-10 17:08:45 +0100

    add views for reviews

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,8 +1,12 @@
+from typing import Any
+
 from django import views
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
+from django.views.generic.base import TemplateView
 
 from reviews.forms import ReviewForm
+from reviews.models import Review
 
 # Create your views here.
 
@@ -21,5 +25,31 @@ class ReviewView(views.View):
         return render(request, "reviews/review.html", {"form": form})
 
 
-def thank_you(request: HttpRequest) -> HttpResponse:
-    return render(request, "reviews/thank_you.html")
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["message"] = "this works"
+        return context
+
+
+class ReviewsListView(TemplateView):
+    template_name = "reviews/review_list.html"
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        reviews = Review.objects.all()
+        context["reviews"] = reviews
+        return context
+
+
+class SingleReviewView(TemplateView):
+    template_name = "reviews/single_review.html"
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        review_id = kwargs["id"]
+        review = Review.objects.get(pk=review_id)
+        context["review"] = review
+        return context
