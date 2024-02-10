b1718e8494bf9ed4f76c0e2e9b8176d5f5690061
tobasqo <s.jasiek.gaming@gmail.com>
2024-02-10 13:45:46 +0100

    add review text and rating to form

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -2,4 +2,16 @@ from django import forms
 
 
 class ReviewForm(forms.Form):
-    user_name = forms.CharField(min_length=1, max_length=100)
+    user_name = forms.CharField(
+        min_length=1,
+        max_length=100,
+        label="Your name",
+        error_messages={
+            "required": "Your name must not be empty!",
+            "max_length": "Please enter a shorter name!",
+        },
+    )
+    review_text = forms.CharField(
+        label="Your feedback", widget=forms.Textarea, max_length=200
+    )
+    rating = forms.IntegerField(label="Your rating", min_value=1, max_value=5)
