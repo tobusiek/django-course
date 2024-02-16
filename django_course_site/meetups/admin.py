65a5895e3d43ab49465d6033c375319c514bc114
tobasqo <s.jasiek.gaming@gmail.com>
2024-02-16 16:03:11 +0100

    add meetup model

--- django_course_site/meetups/admin.py
+++ django_course_site/meetups/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from meetups.models import Meetup
+
 # Register your models here.
+
+admin.site.register(Meetup)
--- django_course_site/meetups/models.py
+++ django_course_site/meetups/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Meetup(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    # location = models.CharField(max_length=50)
