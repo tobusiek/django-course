05100111d3ccfb84d5af0d60a6835a236b2407f9
tobasqo <s.jasiek.gaming@gmail.com>
2024-02-16 14:45:19 +0100

    add redirect to specific metup page

--- django_course_site/meetups/urls.py
+++ django_course_site/meetups/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from meetups import views
 
 urlpatterns = [
-    path("meetups", views.index),
-    path("meetups/<slug:slug>", views.meetup_details),
+    path("meetups", views.index, name="all-meetups"),
+    path("meetups/<slug:slug>", views.meetup_details, name="meetup-details"),
 ]
