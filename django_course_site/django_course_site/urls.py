3e37187e4f48b79621e6716d2fe60fefc463d639
tobasqo <s.jasiek.gaming@gmail.com>
2024-02-19 11:09:06 +0100

    complete course

--- django_course_site/django_course_site/urls.py
+++ django_course_site/django_course_site/urls.py
@@ -19,8 +19,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("meetups.urls")),
+    path("", RedirectView.as_view(url="/meetups")),
+    path("meetups/", include("meetups.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django_course_site/meetups/urls.py
+++ django_course_site/meetups/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from meetups import views
 
 urlpatterns = [
-    path("meetups", views.index, name="all-meetups"),
+    path("", views.index, name="all-meetups"),
     path(
-        "meetups/<slug:slug>/success",
+        "<slug:slug>/success",
         views.confirm_registration,
         name="confirm-registration",
     ),
-    path("meetups/<slug:slug>", views.meetup_details, name="meetup-details"),
+    path("<slug:slug>", views.meetup_details, name="meetup-details"),
 ]
